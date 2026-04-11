5d36e0e119a3ded952d2fa916b944a6bd077e63d
dinesh93426 <tg270786@gmail.com>
2026-04-11 10:59:55 +0530

    chore: enable Vercel to serve uploaded media files

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ app.use(helmet({
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use('/uploads', express.static('uploads'));
+const path = require('path');
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 // Main Route Check
